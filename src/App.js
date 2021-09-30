d606b81480390bbe7cacd17d28328cbb9128dac1
jonahvimahi <jonahvimahi@gmail.com>
2021-09-30 16:04:44 -0600

    almost done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import data from "./data";
+class App extends Component {
+constructor(){
+    super()
+    this.state = {
+      index: 0
+    };
 
-function App() {
+    this.changePagePrevious = this.changePagePrevious.bind(this)
+    this.changePageNext = this.changePageNext.bind(this)
+}
+
+changePagePrevious() {
+  let newIndex = this.state.index
+  if(newIndex === data.length-1){
+    newIndex = 24
+  }else{
+    newIndex--
+  }
+  this.setState({index: newIndex})
+ }
+
+ changePageNext() {
+   let newIndex = this.state.index
+   if(newIndex === data.length-1){
+     newIndex = 0
+   } else{
+     newIndex++
+   }
+   this.setState({index: newIndex})
+  }
+
+
+render() {
+  let lArrow = "\u003C";
+  let rArrow = "\u003E";
+  // console.log(data[this.state.index].name.first);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <h3>Home</h3>
+      </nav>
+      <div id="section">
+        <div>
+          <h1>
+            {data[this.state.index].name.first}{" "}
+            {data[this.state.index].name.last}
+          </h1>
+          <h3>
+            From: {data[this.state.index].city},{" "}
+            {data[this.state.index].country}
+          </h3>
+          <h3>Job Title: {data[this.state.index].title}</h3>
+          <h3>Employer: {data[this.state.index].employer}</h3>
+        </div>
+        <div>
+          <h3>Favorite Movies:</h3>
+          <ol>
+            <li>{data[this.state.index].favoriteMovies[0]}</li>
+            <li>{data[this.state.index].favoriteMovies[1]}</li>
+            <li>{data[this.state.index].favoriteMovies[2]}</li>
+          </ol>
+        </div>
+        <div id="bottom-row">
+          <button onClick={this.changePagePrevious} id="left-arrow">{lArrow}Previous</button>
+          <div id="buttons">
+          <button>Edit</button>
+          <button>Delete</button>
+          <button>New</button>
+          </div>
+          <button onClick={this.changePageNext} id="right-arrow">Next{rArrow}</button>
+        </div>
+      </div>
+    </>
   );
 }
 
+
+
+}
 export default App;
